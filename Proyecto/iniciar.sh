12852ad5153e0590f883765595e450718672550c
AdrianoTisera <adrianotisera@gmail.com>
2023-07-30 22:16:18 -0300

    Removido ByteNet.csv (muy pesado, problemas con LFS)

--- Proyecto/iniciar.sh
+++ Proyecto/iniciar.sh
@@ -9,5 +9,14 @@ source bin/activate
 cd ..
 # Comprobar dependencias
 pip3 install -r requirements.txt
+
+# Preguntar si se quiere extraer la base de datos de ejemplo
+echo "¿Desea extraer la base de datos de ejemplo? (s/n)"
+read respuesta
+if [ $respuesta = "s" ]; then
+    # Extraer base de datos de ejemplo
+    unzip -o Material/example.zip
+fi
+
 # Ejecutar el programa
 python3 main.py
